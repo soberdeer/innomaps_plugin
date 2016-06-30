85c7060447a7e9e2dbf250482cde76aa45d08459
soberdeer <antongraywing@gmail.com>
2016-07-01 02:42:37 +0300

    upload files

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    serverUrl: 'http://localhost:8090/',
+    maxFilesSize: 1024 * 1024 * 5
+};
--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+var config = require('./config.js');
 var path = require('path');
 var express = require('express');
 var app = express();
 var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart();
+var multipartMiddleware = multipart({maxFilesSize: config.maxFilesSize});
 var fs = require('fs');
+var uuid = require('node-uuid');
 
 
 app.use('/static', express.static(path.join(__dirname, 'build/')));
 
 app.use('/upload', multipartMiddleware, function (req, res) {
-    console.log(req.files);
+    var oldName = req.files.file.name;
+    var fileName = uuid.v1();
+    if (oldName.lastIndexOf('.') >= 0) {
+        fileName += oldName.slice(oldName.lastIndexOf('.'), oldName.length);
+    }
     fs.readFile(req.files.file.path, function (err, data) {
-        var newPath = __dirname + '/uploads/' + req.files.file.originalFilename;
+        var newPath = path.join(__dirname, '/uploads/', fileName);
         fs.writeFile(newPath, data, function (err) {
             res.json({
-                success: 1
+                url: path.join(config.serverUrl, '/image', fileName)
             });
         });
     });
+});
+
+app.use('/image/:img', function (req, res) {
+    res.sendFile(path.join(__dirname, '/uploads', req.params.img));
 });
 
 app.use('/', function (req, res) {
